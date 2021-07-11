1158db3e369c6052d14d59f0e6f632a1c7c1a58f
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-07-11 18:55:16 -0500

    Se agrega plantilla de administrador, se crea index para dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ForecastController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,18 +16,24 @@ use App\Http\Controllers\ForecastController;
 
 //Forecasts
 
+    Route::get('/index', function (){
+        return view('admin.index');
+    });
+
     Route::get('/', [ForecastController::class, 'welcome'])->name('welcome');
 
-    Route::get('/free-picks', [ForecastController::class, 'freePicks'])->name('freePicks');
+    Route::get('/free-picks', [ForecastController::class, 'freePicks'])->name('freePicks')->middleware('auth');
 
     Route::get('/premium-register', [ForecastController::class, 'premiumRegister'])->name('premiumRegister');
 
-    Route::get('/premium-picks', [ForecastController::class, 'premiumPicks'])->name('premiumPicks');
+    Route::get('/premium-picks', [ForecastController::class, 'premiumPicks'])->name('premiumPicks')->middleware('auth');
 
     // All CRUD Methods
     Route::resource('forecasts', ForecastController::class);
 
 //END FORECASTS
+
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index')->middleware('auth');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
