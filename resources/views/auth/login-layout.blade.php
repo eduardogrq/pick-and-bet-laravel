35ba88b411241e6f2e690c2b30736ae45904c42d
Eduardo guadalupe Romero quiñonez <eduardogrq@Eduardos-iMac.local>
2021-07-14 00:28:07 -0500

    Se modifica la vista del login

---
+++ resources/views/auth/login-layout.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>@yield('title') - Pick & Bet</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="../../Login_v3/images/icons/favicon.ico"/>
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/fonts/iconic/css/material-design-iconic-font.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/css-hamburgers/hamburgers.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/animsition/css/animsition.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/daterangepicker/daterangepicker.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/css/util.css">
+    <link rel="stylesheet" type="text/css" href="../../Login_v3/css/main.css">
+    <!--===============================================================================================-->
+</head>
+<body>
+
+<div class="limiter">
+
+    @yield('content')
+
+</div>
+
+
+<div id="dropDownSelect1"></div>
+
+<!--===============================================================================================-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!--===============================================================================================-->
+<script src="../../Login_v3/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<!--===============================================================================================-->
+<script src="../../Login_v3/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+<script src="../../Login_v3/vendor/daterangepicker/moment.min.js"></script>
+<script src="../../Login_v3/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+<script src="../../Login_v3/vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+<script src="../../Login_v3/js/main.js"></script>
+
+</body>
+</html>
+
+
+
+
+
+
+{{--<x-guest-layout>--}}
+{{--    <x-jet-authentication-card>--}}
+{{--        <x-slot name="logo">--}}
+{{--            <x-jet-authentication-card-logo />--}}
+{{--        </x-slot>--}}
+
+{{--        <x-jet-validation-errors class="mb-4" />--}}
+
+{{--        @if (session('status'))--}}
+{{--            <div class="mb-4 font-medium text-sm text-green-600">--}}
+{{--                {{ session('status') }}--}}
+{{--            </div>--}}
+{{--        @endif--}}
+
+{{--        <form method="POST" action="{{ route('login') }}">--}}
+{{--            @csrf--}}
+
+{{--            <div>--}}
+{{--                <x-jet-label for="email" value="{{ __('Email') }}" />--}}
+{{--                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />--}}
+{{--            </div>--}}
+
+{{--            <div class="mt-4">--}}
+{{--                <x-jet-label for="password" value="{{ __('Password') }}" />--}}
+{{--                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />--}}
+{{--            </div>--}}
+
+{{--            <div class="block mt-4">--}}
+{{--                <label for="remember_me" class="flex items-center">--}}
+{{--                    <x-jet-checkbox id="remember_me" name="remember" />--}}
+{{--                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>--}}
+{{--                </label>--}}
+{{--            </div>--}}
+
+{{--            <div class="flex items-center justify-end mt-4">--}}
+{{--                @if (Route::has('password.request'))--}}
+{{--                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
+{{--                        {{ __('Forgot your password?') }}--}}
+{{--                    </a>--}}
+{{--                @endif--}}
+{{--                <a href="{{route('register')}}"><small>Regístrate</small></a>--}}
+
+{{--                <x-jet-button class="ml-4">--}}
+{{--                    {{ __('Log in') }}--}}
+{{--                </x-jet-button>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+{{--    </x-jet-authentication-card>--}}
+{{--</x-guest-layout>--}}
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,13 @@
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Login V3</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="../../Login_v3/images/icons/favicon.ico"/>
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/bootstrap/css/bootstrap.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/fonts/iconic/css/material-design-iconic-font.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/animate/animate.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/css-hamburgers/hamburgers.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/animsition/css/animsition.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/select2/select2.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/vendor/daterangepicker/daterangepicker.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/css/util.css">
-    <link rel="stylesheet" type="text/css" href="../../Login_v3/css/main.css">
-    <!--===============================================================================================-->
-</head>
-<body>
+@extends('auth.login-layout')
 
-<div class="limiter">
-    <div class="container-login100" style="background-image: url('../../Login_v3/images/bg-01.jpg');">
+@section('title')
+    Login
+@endsection
+
+@section('content')
+
+    <div class="container-login100" style="background-image: url('../../Login_v3/images/bg-02.jpg');">
         <div class="wrap-login100">
 
             @if (session('status'))
@@ -42,9 +18,11 @@
 
             <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
                 @csrf
+                <a href="{{route('welcome')}}">
 					<span class="login100-form-logo">
 						<i class="zmdi zmdi-landscape"></i>
 					</span>
+                </a>
 
                 <span class="login100-form-title p-b-34 p-t-27">
 						Log in
@@ -68,89 +46,12 @@
 
                 <div class="text-center p-t-90">
                     <small class="text-light">¿No tienes una cuenta?</small><br>
-                    <a class="txt1" href="#">
+                    <a class="txt1" href="{{route('register')}}">
                         Regístrate aquí
                     </a>
                 </div>
             </form>
         </div>
     </div>
-</div>
 
-
-<div id="dropDownSelect1"></div>
-
-<!--===============================================================================================-->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<!--===============================================================================================-->
-<script src="../../Login_v3/vendor/animsition/js/animsition.min.js"></script>
-<!--===============================================================================================-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<!--===============================================================================================-->
-<script src="../../Login_v3/vendor/select2/select2.min.js"></script>
-<!--===============================================================================================-->
-<script src="../../Login_v3/vendor/daterangepicker/moment.min.js"></script>
-<script src="../../Login_v3/vendor/daterangepicker/daterangepicker.js"></script>
-<!--===============================================================================================-->
-<script src="../../Login_v3/vendor/countdowntime/countdowntime.js"></script>
-<!--===============================================================================================-->
-<script src="../../Login_v3/js/main.js"></script>
-
-</body>
-</html>
-
-
-
-
-
-
-{{--<x-guest-layout>--}}
-{{--    <x-jet-authentication-card>--}}
-{{--        <x-slot name="logo">--}}
-{{--            <x-jet-authentication-card-logo />--}}
-{{--        </x-slot>--}}
-
-{{--        <x-jet-validation-errors class="mb-4" />--}}
-
-{{--        @if (session('status'))--}}
-{{--            <div class="mb-4 font-medium text-sm text-green-600">--}}
-{{--                {{ session('status') }}--}}
-{{--            </div>--}}
-{{--        @endif--}}
-
-{{--        <form method="POST" action="{{ route('login') }}">--}}
-{{--            @csrf--}}
-
-{{--            <div>--}}
-{{--                <x-jet-label for="email" value="{{ __('Email') }}" />--}}
-{{--                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />--}}
-{{--            </div>--}}
-
-{{--            <div class="mt-4">--}}
-{{--                <x-jet-label for="password" value="{{ __('Password') }}" />--}}
-{{--                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />--}}
-{{--            </div>--}}
-
-{{--            <div class="block mt-4">--}}
-{{--                <label for="remember_me" class="flex items-center">--}}
-{{--                    <x-jet-checkbox id="remember_me" name="remember" />--}}
-{{--                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>--}}
-{{--                </label>--}}
-{{--            </div>--}}
-
-{{--            <div class="flex items-center justify-end mt-4">--}}
-{{--                @if (Route::has('password.request'))--}}
-{{--                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">--}}
-{{--                        {{ __('Forgot your password?') }}--}}
-{{--                    </a>--}}
-{{--                @endif--}}
-{{--                <a href="{{route('register')}}"><small>Regístrate</small></a>--}}
-
-{{--                <x-jet-button class="ml-4">--}}
-{{--                    {{ __('Log in') }}--}}
-{{--                </x-jet-button>--}}
-{{--            </div>--}}
-{{--        </form>--}}
-{{--    </x-jet-authentication-card>--}}
-{{--</x-guest-layout>--}}
+@endsection
